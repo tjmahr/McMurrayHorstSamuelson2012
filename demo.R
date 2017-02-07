23dfb0e15bed99856e3744b837d94f50616856df
TJ Mahr <mahr@wisc.edu>
2017-02-07 08:00:33 -0600

    trying to figure out why lexicon is not settling

--- demo.R
+++ demo.R
@@ -1,4 +1,5 @@
 library(magrittr)
+
 randomize_weights <- function(weight_matrix, lower, upper) {
   n <- length(weight_matrix)
   draws <- runif(n, lower, upper)
@@ -16,9 +17,11 @@ normalize_vector <- function(xs) {
 }
 
 equalize_vector <- function(xs) {
-  rep(1 / length(xs), length(xs))
+  n <- length(xs)
+  rep(1 / n, n)
 }
 
+# auditory = auditory + m.fbT*auditory .* (decision*aud2dec');
 compute_feedback <- function(input_xs, hidden_xs, weights, scale = 1) {
   feedback <- as.vector(t(weights) %*% hidden_xs) * input_xs
   feedback * scale
@@ -31,23 +34,49 @@ compute_delta_weights <- function(input_xs, hidden_xs, weights, scale = 1) {
   scale * (growth - .5 * decay1 - .5 * decay2)
 }
 
+# Manual way to check above
+compute_delta_weights2 <- function(input_xs, hidden_xs, weights, scale = 1) {
+  deltas <- weights
+
+  for (i in seq_along(input_xs)) {
+    input_x <- input_xs[i]
+
+    for (h in seq_along(hidden_xs)) {
+      input_h <- hidden_xs[h]
+      w <- weights[h, i]
+
+      growth <- input_x * input_h * (1 - w)
+      decay1 <- .5 * (1 - input_x) * input_h * w
+      decay2 <- .5 * input_x * (1 - input_h) * w
+      deltas[h, i] <- growth - decay1 - decay2
+    }
+  }
+  deltas * scale
+}
+
+
 settings <- list(
-  n_wordforms = 20,
-  n_images = 18,
-  n_lexicon = 35,
+  n_wordforms = 35,
+  n_images = 35,
+  n_lexicon = 500,
   weight_init_min = 0,
   weight_init_max = .5,
-  ff_temp = 1,
-  fb_temp = 1,
+  ff_temp = .01,
+  fb_temp = 2,
   inhib_lexicon = 2,
-  inhib_wordform = 1.4,
-  inhib_image = 1.4,
-  learning_rate = .0005
+  inhib_wordform = 1.05,
+  inhib_image = 1.05,
+  learning_rate = .0005,
+  settle_stability = 1e-12
 )
+
+
+
 
 create_network <- function(n_wordforms, n_images, n_lexicon, weight_init_min,
                            weight_init_max, ff_temp, fb_temp, inhib_lexicon,
-                           inhib_wordform, inhib_image, learning_rate) {
+                           inhib_wordform, inhib_image, learning_rate,
+                           settle_stability) {
   layer_wordform <- numeric(n_wordforms)
   layer_image <- numeric(n_images)
   layer_lexicon <- equalize_vector(numeric(n_lexicon))
@@ -78,12 +107,14 @@ create_network <- function(n_wordforms, n_images, n_lexicon, weight_init_min,
       learning_rate = learning_rate,
       inhib_wordform = inhib_wordform,
       inhib_image = inhib_image,
-      inhib_lexicon = inhib_lex,
+      inhib_lexicon = inhib_lexicon,
       ff_temp = ff_temp,
-      fb_temp = fb_temp
+      fb_temp = fb_temp,
+      settle_stability = settle_stability
     ),
     history = list(
-      cycles = 0)
+      cycles = 0,
+      change_in_lexical_activation = 0)
   )
 
   structure(bundle, class = c("mhs_net", "list"))
@@ -107,21 +138,19 @@ set_active_image <- function(network, image) {
 
 run_cycle <- function(network) {
   # Collect input and normalize
-  network$weights$image_lexicon
-  network$weights$wordform_lexicon
-  network$layers$image
-  network$layers$wordform
-  network$layers$lexicon
-  network$params$ff_temp
-
   input_visual <- network$weights$image_lexicon %*% network$layers$image
   input_audio <- network$weights$wordform_lexicon %*% network$layers$wordform
   input_total <- input_visual + input_audio
   update_amount <- network$params$ff_temp * input_total
 
+  last_state <- network$layers$lexicon
   network$layers$lexicon <- (network$layers$lexicon + update_amount) %>%
     power_normalize_vector(network$params$inhib_lexicon) %>%
     as.vector()
+
+  # Need to determine if network has settled
+  change <- network$layers$lexicon - last_state
+  network$history$change_in_lexical_activation <- sum(change ^ 2)
 
   # Feedback to inputs
   fb_image <- compute_feedback(
@@ -177,69 +206,31 @@ cycle_network <- function(network, n_cycles) {
 active_word <- 1
 active_images <- c(1, 2, 4, 6)
 
-n <- do.call(create_network, settings) %>%
+n <- do.call(create_network, settings)
+
+n <- n %>%
   set_active_image(active_images) %>%
   set_active_wordform(active_word)
 
 n <- n %>%
-  cycle_network(1000) %>%
-  set_active_image(c(7, 8, 10)) %>%
-  set_active_wordform(7) %>%
-  cycle_network(1000) %>%
-  set_active_image(c(1, 2, 3, 7, 10)) %>%
-  set_active_wordform(10) %>%
-  cycle_network(1000) %>%
-  set_active_image(c(7, 8, 10)) %>%
-  set_active_wordform(7) %>%
-  cycle_network(1000)
+  cycle_network(10000)
+n
+
+
+
+
+# 1.84e-11 <settle_stability
+#
+# %>%
+#   set_active_image(c(7, 8, 10)) %>%
+#   set_active_wordform(7) %>%
+#   cycle_network(50)
+#
 
 round(n$weights$image_lexicon, 2)
 round(n$weights$wordform_lexicon, 2)
+round(n$layers$image, 2)
+round(n$layers$wordform, 2)
 
-network$layers$lexicon
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # Old pre-network object code
-#
-# # Collect input and normalize
-# visual_input <- weight_image_lexicon %*% layer_image
-# audio_input <- weight_wordform_lexicon %*% layer_wordform
-# total_input <- visual_input + audio_input
-# layer_lexicon <- layer_lexicon + ff_temp * total_input
-# layer_lexicon <- as.vector(power_normalize_vector(layer_lexicon, inhib_lex))
-#
-# # Feedback to inputs
-# fb_image <- as.vector(t(weight_image_lexicon) %*% layer_lexicon) * layer_image
-# layer_image <- layer_image + fb_temp * fb_image
-# layer_image <- power_normalize_vector(layer_image, inhib_image)
-#
-# fb_wordform <- as.vector(t(weight_wordform_lexicon) %*% layer_lexicon)  * layer_wordform
-# layer_wordform <- layer_wordform + fb_temp * fb_wordform
-# layer_wordform <- power_normalize_vector(layer_wordform, inhib_wordform)
-#
-#
-# # Hebbian learning. Not sure yet if matrix multiplication in the function is
-# # correct.
-# d1 <- compute_delta_weights(layer_image, layer_lexicon, weight_image_lexicon)
-# d2 <- compute_delta_weights(layer_wordform, layer_lexicon, weight_wordform_lexicon)
-#
-# weight_image_lexicon <- weight_image_lexicon + learning_rate * d1
-# weight_wordform_lexicon <- weight_wordform_lexicon + learning_rate * d2
-#
-
-
-
-
+n$layers$lexicon
 
